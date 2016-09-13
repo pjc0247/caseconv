32f79b286c0928f64e493e61c250f3726216ea85
Park June Chul <pjc0247@naver.com>
2016-09-13 11:58:10 +0900

    Create caseconv.cs

---
+++ caseconv.cs
@@ -0,0 +1,71 @@
+class CaseTokenizer
+{
+    public static string[] Tokenize(string input)
+    {
+        List<string> tokens = new List<string>();
+        var lastOffset = 0;
+
+        for(int i = 1; i < input.Length; i++)
+        {
+            if (input[i] == '_' ||
+                char.IsUpper(input[i]))
+            {
+                tokens.Add(input.Substring(lastOffset, i - lastOffset));
+
+                if (input[i] == '_')
+                    lastOffset = i + 1;
+                else
+                    lastOffset = i;
+            }
+        }
+        tokens.Add(input.Substring(lastOffset));
+
+        return tokens.Select(x => x.ToLower()).ToArray();
+    }
+}
+
+enum CaseType {
+    Camel,
+    Pascal,
+    Snake,
+    HttpHeader
+}
+class CaseConv
+{
+    public static string Join(string[] input, CaseType type)
+    {
+        var sb = new StringBuilder();
+        var isFirstToken = true;
+
+        foreach(var token in input)
+        {
+            switch(type)
+            {
+                case CaseType.Camel:
+                    sb.Append(isFirstToken ? token : ToUpperFirstCh(token));
+                    break;
+                case CaseType.Pascal:
+                    sb.Append(ToUpperFirstCh(token));
+                    break;
+                case CaseType.Snake:
+                    sb.Append(token + "_");
+                    break;
+                case CaseType.HttpHeader:
+                    sb.Append(ToUpperFirstCh(token) + "-");
+                    break;
+            }
+
+            isFirstToken = false;
+        }
+
+        if (type == CaseType.Snake || type == CaseType.HttpHeader)
+            sb.Remove(sb.Length - 1, 1);
+
+        return sb.ToString();
+    }
+
+    private static string ToUpperFirstCh(string input)
+    {
+        return char.ToUpper(input.First()) + input.Substring(1);
+    }
+}
